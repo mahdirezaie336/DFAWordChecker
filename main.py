8aeb69b71fe7e67910f3b47cf7262db07cafa13b
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-11-30 10:20:02 +0330

    Create a 2nd machine to test

--- main.py
+++ main.py
@@ -29,19 +29,42 @@ class NDFA:
 
 
 def main():
-    all_states = {'S00', 'S01', 'S02'}
-    alphabet = {'a', 'b', 'c'}
-    init_state = 'S00'
-    final_states = {'S02'}
-    transitions = [('S00', 'a', 'S00'), ('S00', 'b', 'S01'), ('S01', 'a', 'S01'), ('S01', 'b', 'S02'),
-                   ('S02', 'a', 'S02'), ('S02', 'b', 'S02')]
+    machine1_all_states = {'S00', 'S01', 'S02'}
+    machine1_alphabet = {'a', 'b', 'c'}
+    machine1_init_state = 'S00'
+    machine1_final_states = {'S02'}
+    machine1_transitions = [('S00', 'a', 'S00'), ('S00', 'b', 'S01'),
+                            ('S01', 'a', 'S01'), ('S01', 'b', 'S02'),
+                            ('S02', 'a', 'S02'), ('S02', 'b', 'S02')]
     #   _______        _______        _______
     #   | S00 | --b--> | S01 | --b--> | S02 |
     #   -------        -------        -------
-    machine = NDFA(all_states, init_state, final_states, alphabet, *transitions)
+    #    ^-a--          ^-a--          ^-ab-
+    # Accepts a word with at least two 'b'
+    machine1 = NDFA(machine1_all_states, machine1_init_state, machine1_final_states,
+                    machine1_alphabet, *machine1_transitions)
 
-    print('aaaabbaaaa' in machine)
-    print('abaaaaaa' in machine)
+    print('aaaabbaaaa' in machine1)
+    print('abaaaaaa' in machine1)
+
+    machine2_all_states = {'S00', 'S01', 'S02'}
+    machine2_alphabet = {'a', 'b', 'c'}
+    machine2_init_state = 'S00'
+    machine2_final_states = {'S02'}
+    machine2_transitions = [('S00', 'a', 'S00'), ('S00', 'b', 'S01'),
+                            ('S01', 'a', 'S00'), ('S01', 'b', 'S02'),
+                            ('S02', 'a', 'S02'), ('S02', 'b', 'S02')]
+    #    v-a--
+    #   _______        _______        _______
+    #   | S00 | --b--> | S01 | --b--> | S02 |
+    #   -------        -------        -------
+    #    ^--------a---------           ^-ab-
+    # Accepts a word with at least one 'bb'
+    machine2 = NDFA(machine2_all_states, machine2_init_state, machine2_final_states,
+                    machine2_alphabet, *machine2_transitions)
+
+    print('aaaabaabaa' in machine2)
+    print('abbaaaaa' in machine2)
 
 
 if __name__ == '__main__':
