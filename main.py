89eb0d81603dec7e39684e86241281d12581466b
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-11-30 10:09:42 +0330

    Create a machine to test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class NDFA:
 
-    def __init__(self, all_states: list['str'],
+    def __init__(self, all_states: set['str'],
                  init_state: str,
                  final_states: set['str'],
-                 alphabet: list['str'],
+                 alphabet: set['str'],
                  *transitions):
 
         self.all_states = all_states
@@ -17,7 +17,7 @@ class NDFA:
             self.transitions[item[0]][item[1]] = item[2]
 
     def __contains__(self, item):
-        if type(item) != 'str':
+        if type(item) != str:
             raise TypeError('Type must be "str"')
         current_state = self.init_state
         for letter in item:
@@ -26,3 +26,23 @@ class NDFA:
             except KeyError:
                 return False
         return current_state in self.final_states
+
+
+def main():
+    all_states = {'S00', 'S01', 'S02'}
+    alphabet = {'a', 'b', 'c'}
+    init_state = 'S00'
+    final_states = {'S02'}
+    transitions = [('S00', 'a', 'S00'), ('S00', 'b', 'S01'), ('S01', 'a', 'S01'), ('S01', 'b', 'S02'),
+                   ('S02', 'a', 'S02'), ('S02', 'b', 'S02')]
+    #   _______        _______        _______
+    #   | S00 | --b--> | S01 | --b--> | S02 |
+    #   -------        -------        -------
+    machine = NDFA(all_states, init_state, final_states, alphabet, *transitions)
+
+    print('aaaabbaaaa' in machine)
+    print('abaaaaaa' in machine)
+
+
+if __name__ == '__main__':
+    main()
